66da3d3a2aa1f34a78f0a0e468375c3751933426
Gabriel Naves <gabri.navess@gmail.com>
2021-08-29 07:28:58 -0300

    Pieces now get placed on top of each other in the grid

--- Assets/_Scripts/Tetris Gameplay/GameGrid.cs
+++ Assets/_Scripts/Tetris Gameplay/GameGrid.cs
@@ -12,13 +12,22 @@ public class GameGrid : MonoBehaviour
     [SerializeField] private bool showGizmos;
     [SerializeField] private Color gizmoColor;
 
+    private int width, height;
+    private Transform[,] grid;
+
     private void Awake()
     {
         instance = Singleton.Setup(this, instance) as GameGrid;
+        width = Mathf.RoundToInt(gridBounds.size.x);
+        height = Mathf.RoundToInt(gridBounds.size.y);
+        grid = new Transform[width, height];
     }
 
     public bool IsPositionValid(Vector3 pos)
     {
+        var gridPos = WorldPosToGridCoordinate(pos);
+        if (grid[gridPos.x, gridPos.y] != null)
+            return false;
         return pos.x >= gridBounds.min.x && pos.x <= gridBounds.max.x &&
                pos.y >= gridBounds.min.y;
     }
@@ -26,6 +35,17 @@ public class GameGrid : MonoBehaviour
     public void AddCubeToGrid(Transform cube)
     {
         cube.SetParent(transform);
+        var gridPos = WorldPosToGridCoordinate(cube.position);
+        grid[gridPos.x, gridPos.y] = cube;
+    }
+
+    private Vector2Int WorldPosToGridCoordinate(Vector3 pos)
+    {
+        return new Vector2Int()
+        {
+            x = Mathf.Clamp(Mathf.FloorToInt(pos.x - gridBounds.min.x), 0, width - 1),
+            y = Mathf.Clamp(Mathf.FloorToInt(pos.y - gridBounds.min.y), 0, height - 1)
+        };
     }
 
     private void OnDrawGizmos()
